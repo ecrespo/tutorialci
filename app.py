d066f810e0707ef815464451bbb2c09074ee1ce1
Ernesto Crespo <ecrespo@gmail.com>
2020-03-14 14:41:00 -0400

    Reformateado de app

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask 
- 
-app = Flask(__name__) 
+from flask import Flask
 
-@app.route('/<name>') 
-def hello_world(name: str) -> str: 
+
+@app.route("/<name>")
+def hello_world(name: str) -> str:
     return hello_name(name)
+
 
 def hello_name(name: str) -> str:
     return f"hello, {name}"
 
-if __name__ == '__main__': 
+
+if __name__ == "__main__":
+    app = Flask(__name__)
     app.run(debug=False)
