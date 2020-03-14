be47ff30bb39df0ee67c01ae53b3d77afacc6655
Ernesto Crespo <ecrespo@gmail.com>
2020-03-14 14:00:28 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask 
+ 
+app = Flask(__name__) 
+
+@app.route('/<name>') 
+def hello_world(name: str) -> str: 
+    return hello_name(name)
+
+def hello_name(name: str) -> str:
+    return f"hello, {name}"
+
+if __name__ == '__main__': 
+    app.run(debug=False)
