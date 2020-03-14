f6901b6e0e71dce9f5afeab824c705b6360d2426
Ernesto Crespo <ecrespo@gmail.com>
2020-03-14 14:55:18 -0400

    formateado app

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+app = Flask(__name__)
+
 
 @app.route("/<name>")
 def hello_world(name: str) -> str:
@@ -11,5 +13,4 @@ def hello_name(name: str) -> str:
 
 
 if __name__ == "__main__":
-    app = Flask(__name__)
     app.run(debug=False)
